de822685138a6082bad3f074b161a2042832ff05
thomashalo11 <thomastresca11@gmail.com>
2023-10-05 13:37:59 +0200

    update

--- prima_app/views.py
+++ prima_app/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, "homepage.html")
+def variabili(request):
+    context = {
+        'var1': 'Prima variabile',
+        'var2':  'Seconda variabile',
+        'var3': 'Terza variabile'
+    }
+    return render(request, "variabili.html", context)
