dc88714bd74c5cab33e1fb2e12b42741eecc373f
thomashalo11 <thomastresca11@gmail.com>
2023-10-05 13:52:45 +0200

    update

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from prima_app.views import homepage
+from prima_app.views import homepage, welcome, lista, chi_siamo, variabili
 
 app_name="prima_app"
 urlpatterns=[
     path('', homepage, name='homepage'),
+    path('', welcome, name='welcome'),
+    path('', lista, name='lista'),
+    path('', chi_siamo, name="chi_siamo"),
+    path('', variabili, name="variabili")
 ]
--- prima_app/views.py
+++ prima_app/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, "homepage.html")
+def welcome(request):
+    return render(request, "")
+def lista(request):
+    return render(request, "")
+def chi_siamo(request):
+    return render(request, "")
+def variabili(request):
+    return render(request, "")
+
 def variabili(request):
     context = {
         'var1': 'Prima variabile',
