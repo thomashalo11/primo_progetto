8d49b4d458728445e16fd5249a1fae17089191a8
thomashalo11 <thomastresca11@gmail.com>
2023-10-13 12:59:52 +0200

    passo 8 : add index prima_app

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from prima_app.views import homepage, welcome, lista, chi_siamo, variabili
+from prima_app.views import homepage, welcome, lista, chi_siamo, variabili,index
 
 app_name="prima_app"
 urlpatterns=[
+    path('', index, name='index'),
     path('homepage', homepage, name='homepage'),
     path('welcome', welcome, name='welcome'),
     path('lista', lista, name='lista'),
--- prima_app/views.py
+++ prima_app/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
 def homepage(request):
     return render(request, "homepage.html")
 def welcome(request):
-    return render(request, "")
+    return render(request, "welcome.html")
 def lista(request):
-    return render(request, "")
+    return render(request, "lista.html")
 def chi_siamo(request):
-    return render(request, "")
+    return render(request, "chisiamo.html")
 def variabili(request):
-    return render(request, "")
+    return render(request, "variabili.html")
 
 def variabili(request):
     context = {
