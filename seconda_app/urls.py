ec0c35874d1d702c527564ab20f2b3e6b33e7f45
thomashalo11 <thomastresca11@gmail.com>
2023-10-27 10:26:49 +0200

    Passo 10

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -4,6 +4,6 @@ from seconda_app.views import es_if, index
 app_name = "seconda_app"
 urlpatterns=[
 
-    path('index', index, name='index.html'),
+    path('', index, name='index'),
     path('es_if', es_if, name='es_if'),
 ]
