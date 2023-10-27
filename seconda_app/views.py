21545805c8fd9a3e1b712036b5bef7bd25f160c3
thomashalo11 <thomastresca11@gmail.com>
2023-10-27 09:42:14 +0200

    Passo 10 (fix)

--- seconda_app/views.py
+++ seconda_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
 def es_if(request):
     context = {
         'var1' : 200,
