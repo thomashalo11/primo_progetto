18439d0890dd47d2c16c1154e097bb6ef53d8a01
thomashalo11 <thomastresca11@gmail.com>
2023-10-27 10:51:50 +0200

    Passo 11

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from seconda_app.views import es_if, index2
+from seconda_app.views import es_if, index2, if_else_elif
 
 app_name = "seconda_app"
 urlpatterns=[
 
     path('index2', index2, name='index2'),
     path('es_if', es_if, name='es_if'),
+    path('if_else_elif', if_else_elif, name='if_else_elif')
 ]
--- seconda_app/views.py
+++ seconda_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index2(request):
     return render(request, "index2.html")
+
 def es_if(request):
     context = {
         'var1' : 200,
@@ -10,3 +11,11 @@ def es_if(request):
         'var3' : 300
     }
     return render(request, "es_if.html", context)
+
+def if_else_elif(request):
+    context = {
+        'var1' : 100,
+        'var2' : 100.0,
+        'var3' : 100.50,
+    }
+    return render(request, "if_else_elif.html", context)
