c80fec9b252b893df382c7d22b96318d564cdeca
thomashalo11 <thomastresca11@gmail.com>
2023-11-03 10:56:09 +0100

    Passo 15

--- prova_pratica_1/views.py
+++ prova_pratica_1/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,10 +6,38 @@ def index_prova_pratica(request):
     return render(request, "index_prova_pratica.html")
 
 def somma(request):
-    return render(request, "somma.html") # Se poi aggiungo variabili in somma.html aggiungere un context
+    num1 = random.randint(0,10)
+    num2 = random.randint(0,10)
+    nSomma = num1 + num2
+    context = {
+        'num1' : num1,
+        'num2' : num2,
+        'nSomma' : nSomma,
+    }
+    return render(request, "somma.html", context) # Se poi aggiungo variabili in somma.html aggiungere un context
 
 def media(request):
-    return render(request, "media.html") # Se poi aggiungo variabili in media.html aggiungere un context
+    numeri = []
+    n = 30
+    somma = 0
+    # Creazione lista
+    for i in range(n):
+        numeri.append(random.randint(1,10))
+    # Somma
+    for i in range(len(numeri)):
+        somma += numeri[i]
+    # Media
+    media = somma / len(numeri)
+    
+    context = {
+        'numeri' : numeri,
+        'somma' : somma,
+        'media' : media,
+    }
+    return render(request, "media.html", context) # Se poi aggiungo variabili in media.html aggiungere un context
 
 def voti(request):
-    return render(request, "voti.html") # Se poi aggiungo variabili in voti.html aggiungere un context
+    context = {
+        'voto' : {"studente1":8, "studente2":7,"studente3":5}
+    }
+    return render(request, "voti.html", context) # Se poi aggiungo variabili in voti.html aggiungere un context
