c70bea8d03289e05b7b84c98d3d408cf9421ba9f
thomashalo11 <thomastresca11@gmail.com>
2023-11-10 08:58:10 +0100

    Passo 16

---
+++ news/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+# Create your models here.
+
+class Giornalista(models.Model):
+    """ Il modello generico di un giornalista """
+    nome = models.CharField(max_length=20)
+    cognome = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome + " " + self.cognome
+
+class Articolo(models.Model):
+    """ Il modello generico di un articolo di news """
+    titolo = models.CharField(max_length=100)
+    contenuto = models.TextField()
+    giornalista = models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name="articoli")
+
+    def __str__(self):
+        return self.titolo
--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'prima_app',
     'seconda_app',
+    'news',
 ]
 
 MIDDLEWARE = [
