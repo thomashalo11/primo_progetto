cf388467221f7fd4011a1f982034a053632c1c4f
thomashalo11 <thomastresca11@gmail.com>
2023-11-24 10:26:36 +0100

    Template e Boostrap

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -5,10 +5,10 @@ app_name="prima_app"
 urlpatterns=[
 
     path('', index_root, name='index_root'),
-    path('index', index, name='index'),
-    path('homepage', homepage, name='homepage'),
-    path('welcome', welcome, name='welcome'),
-    path('lista', lista, name='lista'),
-    path('chi_siamo', chi_siamo, name="chi_siamo"),
-    path('variabili', variabili, name="variabili")
+    path('index/', index, name='index'),
+    path('homepage/', homepage, name='homepage'),
+    path('welcome/', welcome, name='welcome'),
+    path('lista/', lista, name='lista'),
+    path('chi_siamo/', chi_siamo, name="chi_siamo"),
+    path('variabili/', variabili, name="variabili")
 ]
